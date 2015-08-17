d4e8d188947efe23125843231f5b9fb791651ff0
Stacy Fabian <stacy.fabian@gmail.com>
2015-08-16 22:49:45 -0700

    begin delivery options section build

--- js/app.js
+++ js/app.js
@@ -17,8 +17,9 @@ submitButton.addEventListener('click', submitClick);
 // Next Steps:
 
 // Send array values to JSON - CHECK
-// Force value type on fields
+// Different FTP config sections
 // Dropdown for sponsors
+// Import sponsor options from "database"
 // Active search for sponsors
-// Import sponsor optionsfrom "database"
+// Force value type on fields
 // 
