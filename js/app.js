9036738ecdf346fd7776e5bdc731d0981626e95f
Stacy Fabian <stacy.fabian@gmail.com>
2015-08-19 23:20:01 -0700

    toggle FTP Config Sections

--- js/app.js
+++ js/app.js
@@ -1,21 +1,41 @@
 // decoupling our proof of concept to prove maximum ROI for scalability potential
 
-var data = {};
 // =====
-// Assign classes to FTP Sections for use
+// Assign event listeners to FTP Delivery Option Buttons
 // =====
 
 $(document).ready(function(){
 
-	deliverSiteManager.addEventListener('click', function(){
-		$("siteManagerDeliveryConfig").removeClass('hidden');
-		$(h4).removeClass('hidden');
-		$("siteManagerDeliveryConfig").addClass('selected');
-		console.log("selected applied to siteManagerDeliveryConfig")
-		//alert("The looping function should go here.");
-	});
+	document.getElementById("deliverSiteManagerButton").addEventListener('click',siteManagerDelivery);
+
+	document.getElementById("sftpPullDeliverButton").addEventListener('click', SFTPPullDelivery);
+
+	document.getElementById("sftpPushDeliverButton").addEventListener('click', SFTPPushDelivery);
 
 });
+
+// =====
+// Apply or Toggle "Hidden" class to toggle FTP Delivery Sections
+// =====
+
+var siteManagerDelivery = function(){
+		$(".siteManagerDeliveryConfig").toggleClass('hidden');
+		$(".sftpPullDeliveryConfig").addClass('hidden');
+		$(".sftpPushDeliveryConfig").addClass('hidden');
+	};
+
+var SFTPPullDelivery = 	function(){
+		$(".sftpPullDeliveryConfig").toggleClass('hidden');
+		$(".siteManagerDeliveryConfig").addClass('hidden');
+		$(".sftpPushDeliveryConfig").addClass('hidden');
+	};
+
+var SFTPPushDelivery = function(){
+		$(".sftpPushDeliveryConfig").toggleClass('hidden');
+		$(".siteManagerDeliveryConfig").addClass('hidden');
+		$(".sftpPullDeliveryConfig").addClass('hidden');
+	};
+
 
 // NEXT STEPS:
 // 1. Get elements to apply and remove css class for visibility after button click
@@ -23,6 +43,11 @@ $(document).ready(function(){
 
 // =====
 // Save General BDE Data into object and convert to JSON
+// Just recreate in different sections.
+//		1. Collect FTP as own object data set
+//		Or
+// 		2. Only add fields with values into single BDE Config Object with delivery
+//		   info key:value pairs
 // =====
 
 var data = {};
@@ -45,10 +70,11 @@ saveButton.addEventListener('click', saveClick);
 
 // Send array values to JSON - CHECK
 // Dropdown for sponsors
+
+
+// NICE TO HAVE:
+
 // Import sponsor options from "database"
 // Active search for sponsors
 // Force value type on fields (email)
-
-
-
 
